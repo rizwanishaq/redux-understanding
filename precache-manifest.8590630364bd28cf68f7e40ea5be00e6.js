77c51123e2e14ec19efec4876de0b60ecb4e1c1d
rizwanishaq <rizwanishaq@gmail.com>
2019-12-10 11:39:40 +0100

    Updates

---
+++ precache-manifest.8590630364bd28cf68f7e40ea5be00e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a197de4b20af074f842d1d808662b384",
+    "url": "/redux-understanding/index.html"
+  },
+  {
+    "revision": "a852d3baa0c95693fdcb",
+    "url": "/redux-understanding/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d2b4d4772d43fb7b06da",
+    "url": "/redux-understanding/static/js/2.c3bbcd33.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/redux-understanding/static/js/2.c3bbcd33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a852d3baa0c95693fdcb",
+    "url": "/redux-understanding/static/js/main.80d277d9.chunk.js"
+  },
+  {
+    "revision": "53020389f948cb892f3f",
+    "url": "/redux-understanding/static/js/runtime-main.9c30acd8.js"
+  }
+]);
